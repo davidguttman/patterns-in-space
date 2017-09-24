6e90fc319f8b4806cdc38cb0dda52b0be8b015e9
David Guttman <david@davidguttman.com>
2017-09-24 07:32:43 -0700

    fiat lux

---
+++ index.js
@@ -0,0 +1,98 @@
+
+var h = require('hyperscript')
+var TAU = 2 * Math.PI
+
+document.body.style.background = 'rgb(20, 20, 20)'
+
+var stage = createStage()
+document.body.appendChild(stage)
+var poly = createPolygon({radius: 250, n: 12})
+stage.appendChild(poly.el)
+poly.update()
+
+// window.requestAnimationFrame(updateLoop)
+
+function updateLoop () {
+  // window.requestAnimationFrame(updateLoop)
+}
+
+function createPolygon (opts) {
+  var nPoints = opts.n
+
+  var ns = 'http://www.w3.org/2000/svg'
+  var viewBox = [0, 0, 1, 1].join(' ')
+
+  var parent = document.createElement('div')
+  parent.style.width = '100%'
+  parent.style.height = '100%'
+  parent.innerHTML = `
+    <svg xmlns='${ns}'
+      viewBox='${viewBox}'
+      width='100%'
+      height='100%'
+      stroke='none'
+      fill='none'>
+    </svg>
+  `
+
+  var svg = parent.children[0]
+
+  var points = new Array(nPoints).fill(0).map(function (point, i) {
+    point = point || {}
+    point.theta = i / nPoints * TAU
+    point.x = 0.5 * Math.cos(point.theta) + 0.5
+    point.y = 0.5 * Math.sin(point.theta) + 0.5
+    return point
+  })
+
+  console.log('points', points)
+
+  var lines = points.map(function (point, i) {
+    var prev = points[i - 1] || points[points.length - 1]
+    return line(point, prev)
+  })
+
+  console.log('lines', lines)
+
+  return {
+    el: parent,
+    svg: svg,
+    points: points,
+    lines: lines,
+    update: function () {
+      lines.forEach(function (line, i) {
+        var child = svg.children[i]
+        if (!child) {
+          child = document.createElementNS(ns, 'line')
+          child.setAttributeNS(null, 'stroke', 'none')
+          svg.appendChild(child)
+        }
+
+        child.setAttributeNS(null, 'x1', line.x1)
+        child.setAttributeNS(null, 'y1', line.y1)
+        child.setAttributeNS(null, 'x2', line.x2)
+        child.setAttributeNS(null, 'y2', line.y2)
+        child.setAttributeNS(null, 'stroke', 'white')
+        child.setAttributeNS(null, 'stroke-width', '0.005')
+      })
+    }
+  }
+}
+
+function line (pa, pb) {
+  return {x1: pa.x, y1: pa.y, x2: pb.x, y2: pb.y}
+}
+
+function createStage (w, h) {
+  var stage = document.createElement('div')
+  stage.style.position = 'absolute'
+  stage.style.top = '10%'
+  stage.style.left = '10%'
+  stage.style.width = '80%'
+  stage.style.height = '80%'
+  return stage
+}
+
+function mod (a, n) {
+  return ((a % n) + n) % n
+}
